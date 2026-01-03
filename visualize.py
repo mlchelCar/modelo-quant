e9685b15b75f79076dd4a9f3e3f0328a635a6112
michel <89163924+mlchelCar@users.noreply.github.com>
2026-01-03 14:40:26 -0300

    Saving

--- visualize.py
+++ visualize.py
@@ -1057,4 +1057,72 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
+
+'''
+Todo
+
+Resolver situacao dos simbolos                                       OK
+Calcular Moving Average Crossings                                    OK
+Calcular Volume Profile                                              OK
+Plotar POC                                                           OK
+Add entry signals                                                    OK
+Fix decide_entry_direction                                           OK
+Calculate result from each entrie                                    OK
+Fix symbol with duplicate entries                                    OK
+Fix sharpe calculation                                               OK
+Add Costs                                                            OK
+Add Slippage                                                         OK
+Plot Entries stop and tp                                             OK
+Fix POC step (0.0005)                                                OK
+Fix moving Average  Mistake                                          OK
+Make Moving Average Fix Clean                                        OK
+Fix Sharpe Calculation                                               OK
+Review Trade Closing                                                 OK
+Review for other possible mistakes                                   OK
+Limit trades to same contract as vp                                  OK
+Add Metric Average Trade Duration                                    -
+Add Metric Avg Win                                                   OK
+Add Metric Avg Loss                                                  OK
+Add Metric Number of days                                            OK
+Fix metrics Total Trades                                             OK
+Control Trade Duration                                               OK
+Optimize load_data function
+Optimizacao: fazer compute_data e return from trades para todas variantes ao mesmo tempo
+Optimizar memoria
+Otimizacao: detect entries lopando em candles apartir do B
+Optimize volume profile function                                     OK
+Save output                                                          OK
+Implement week or holiday exit
+Generate p&l graph function                                         
+Week profit visualization
+Generate CI per rolling graph
+Show Rolling Division on visualization
+Add 1 year, 3 months, 1 month returns
+see win and loss distribution
+filter win and loss distribution based on metrics
+Fit and Test separated (out of sample)                               OK
+Fit and Test rolling out of sample                                   OK
+Add final date                                                       OK
+Returns in %                                                         OK
+Update Capital with trade results                                    OK
+In Sample Permutation Test
+Permutate_candles function
+Position Sizing with volatility standardization                      OK
+Ploting Standard Deviation                                           OK
+Stop size based on ATR                                               OK
+Fix instrument specifics(tick size, tick value, costs)               OK
+Fix volality in % not being handled                                  OK
+Handle 0 contracts situations                                        OK
+Test keeping position open                                           OK
+Fix Contract Switch to match tradingview
+Trades bleeding across Contracts Add Switch                          OK
+Volatility scaling: you’re treating 1-hour STD as daily STD          OK
+Using Daily ATR                                                      OK
+Compute std on daily candles
+Remove Zero-return days?
+Avg Win / Avg Loss are wrong                                         OK
+Fix Compute Data                                                     OK
+Tralling Stop instead of take profit
+Position Sizing with Forecast Value
+Combine M6E, MCL, etc daily returns to see the metrics togheter
+'''
